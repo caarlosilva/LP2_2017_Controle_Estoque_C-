5c498060fafeb60bf8960cfa1882787392f39c94
Fabricio Rocha <fabricio10.90@hotmail.com>
2017-10-01 15:29:32 -0300

    Modificado idFornecedor em Produtos e Fornecededor
    idFornecedor estava comentado em produtos e fornecedor.

--- DBControlX.sql
+++ DBControlX.sql
@@ -1,10 +1,11 @@
-﻿
+﻿--create database controlx;
+--use controlx;
 
 CREATE TABLE fornecedor (
   id int NOT NULL AUTO_INCREMENT,
   nome varchar(64) DEFAULT NULL,
-  cnpj bigint(15) DEFAULT NULL,
- tel1 bigint(20) DEFAULT NULL,	
+  cnpj bigint(15) DEFAULT NULL, --É 100X MELHOR FAZERMOS ESSAS PORRA COM BIGINT COM VARCHAR
+  tel1 bigint(20) DEFAULT NULL,	
   tel2 bigint(20) DEFAULT NULL,
   cep bigint(10) DEFAULT NULL,
   num bigint(20) DEFAULT NULL,
@@ -21,5 +22,8 @@ CREATE TABLE produtos (
   id int NOT NULL AUTO_INCREMENT,
   nome varchar(64) DEFAULT NULL,
   preco double DEFAULT NULL,
-  qntd int DEFAULT NULL, PRIMARY KEY (id)
-);
+  qntd int DEFAULT NULL,
+ idFornecedor int,
+  PRIMARY KEY (id)
+, FOREIGN KEY (idFornecedor) REFERENCES fornecedor (id)
+ );
