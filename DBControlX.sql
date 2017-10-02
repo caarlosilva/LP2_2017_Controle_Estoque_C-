a4545e2cc00ad58a9d0c1a5e13fe36837647bac4
Carlos Silva <caarlos.silva06@gmail.com>
2017-10-02 00:45:06 -0300

    Update DBControlX.sql

--- DBControlX.sql
+++ DBControlX.sql
@@ -1,14 +1,14 @@
-﻿--create database controlx;
---use controlx;
+create database controlx;
+use controlx;
 
 CREATE TABLE fornecedor (
   id int NOT NULL AUTO_INCREMENT,
   nome varchar(64) DEFAULT NULL,
-  cnpj bigint(15) DEFAULT NULL, --É 100X MELHOR FAZERMOS ESSAS PORRA COM BIGINT COM VARCHAR
-  tel1 bigint(20) DEFAULT NULL,	
-  tel2 bigint(20) DEFAULT NULL,
-  cep bigint(10) DEFAULT NULL,
-  num bigint(20) DEFAULT NULL,
+  cnpj varchar(15) DEFAULT NULL, 
+  tel1 varchar(11) DEFAULT NULL,	
+  tel2 varchar(11) DEFAULT NULL,
+  cep varchar(10) DEFAULT NULL,
+  num int(6) DEFAULT NULL,
   rua varchar(64) DEFAULT NULL,
   comp varchar(32) DEFAULT NULL,
   bairro varchar(64) DEFAULT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE produtos (
   nome varchar(64) DEFAULT NULL,
   preco double DEFAULT NULL,
   qntd int DEFAULT NULL,
- idFornecedor int,
-  PRIMARY KEY (id)
-, FOREIGN KEY (idFornecedor) REFERENCES fornecedor (id)
+  idFornecedor int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (idFornecedor) REFERENCES fornecedor (id)
  );
